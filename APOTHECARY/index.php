10348d34ac5711962dac5933a03d0aab03354a35
Abishek K S <99885858+Abishek093@users.noreply.github.com>
2022-10-23 11:28:45 +0530

    Add files via upload

---
+++ APOTHECARY/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Apothecary</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <section class="main">
+        <nav>
+            <a href="#" class="logo">
+               <img src="images/Appotheryshort.png">    
+             </a>
+
+             <ul class="Menu">
+                <li><a href="#">Home
+        </nav>
+    </sectin>
+</body>
+</html> 
